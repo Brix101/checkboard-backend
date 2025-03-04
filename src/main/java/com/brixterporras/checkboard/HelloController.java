b8ff20981aab5f78102b7c3ea7c2ce0923ef31a0
Brixter Porras <brixterporras@gmail.com>
2025-03-04 19:48:55 +0800

    feat: added hello world controller

--- src/main/java/com/brixterporras/checkboard/HelloController.java
+++ src/main/java/com/brixterporras/checkboard/HelloController.java
@@ -2,6 +2,7 @@ package com.brixterporras.checkboard;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld(){
-        return "Hello, World!";
+    public String helloWorld(@RequestParam(name = "name", defaultValue = "World") String name){
+        return "Hello, " + name + "!";
     }
 }
